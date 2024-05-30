6a257010a0064536fadb8922c189e71e6cccb15d
Rosalie Duteuil <rosa-lee@live.fr>
2024-05-30 11:12:34 +0200

    Révision du shell qui gère les pipes - création de exec_pipeline pour alléger le main - fork réalisé en amont du traitement (l'ensemble de la pipeline est gérée par un sous-shell) - amélioration de la gestion des erreurs (exit et/ou valeurs de retour plus cohérents)

--- shell.c
+++ shell.c
@@ -1,5 +1,5 @@
 // *******************************************************
-// Version ..... : V1.0 du 27/05/2024
+// Version ..... : V1.1 du 30/05/2024
 // 
 // Fonctionnalités :
 // 		- les pipes
@@ -8,11 +8,13 @@
 
 # include "sys.h"
 #include <string.h>
+#include <ctype.h>	// pour isspace()
 
 int decouper(char *, char *, char *[], int);
 int exec_pipeline(char *);
-int lance_cmd(char *[]);
-int traite_pipe(int, char *);
+int lance_cmd(char *);
+int traite_pipe(char * []);
+int est_vide(char *);
 
 enum {
 	MaxLigne = 1024, 			// longueur max d'une ligne de commandes
@@ -22,104 +24,112 @@ enum {
 	MaxPipes = 128,				// nbre max de commandes séparées par " | " sur une ligne
 };
 
-# define PROMPT "V1.0 "
+# define PROMPT "V1.1 "
 char * path[MaxDirs];			// liste des répertoires de PATH (globale ???)
 
 int main(int argc, char * argv[]){
 	
 	char ligne[MaxLigne];			// la dernière ligne entrée par l'utilisateur
+	int tmp; 
 
-	/* Decouper UNE COPIE de PATH en repertoires */
+	/* Découper UNE COPIE de PATH en repertoires */
 	decouper(strdup(getenv("PATH")), ":", path, MaxDirs);
 
-	/* Lire et traiter chaque ligne de commande */
-	for (printf(PROMPT); fgets(ligne, sizeof ligne, stdin) != 0; printf(PROMPT))
-		exec_pipeline(ligne);
+	/* Lire et traiter chaque ligne saisie dans le terminal */
+	for (printf(PROMPT); fgets(ligne, sizeof ligne, stdin) != 0; printf(PROMPT)){
+		
+		if (est_vide(ligne)){
+			fprintf(stderr, "Commande vide ! (0)\n");
+			continue;}		// ligne vide
 
+		/* lancement du processus fils qui va exécuter la pipeline */
+		if ((tmp = fork()) < 0){
+			perror("fork");
+			continue;}
+
+		/* parent : attendre la fin de l'enfant (ou pas) */
+		if (tmp != 0){
+			while(wait(0) != tmp);
+			/* Récupérer le statut de retour ? Faire waitpid(&status) ou un truc du genre ?*/
+			continue ;} // renvoyer le bon statut
+
+		/* enfant : prend en charge  la pipeline */
+		exec_pipeline(ligne);	// pour l'instant, une seule pipeline
+	}
 	printf("Bye\n");
 	return 0;
 }
 
-/* Exécute une pipeline (une série de commandes séparées par des | */
+/* Exécute une pipeline (une série de commandes séparées par des "|" ) */
 int exec_pipeline(char * pipeline){
-	char * mot[MaxMot];				// liste des mots de la ligne saisie
 	char * liste_cmd[MaxPipes];		// la ou les (en cas de pipe(s)) commande(s) de la ligne
-	int tmp, i;
-	int in = 0;						// entrée à utiliser pour le prochain processus
+	int i = 0;
 
 	/* Séparer les différentes commandes en cas de pipe */
-		decouper(pipeline, "|\n", liste_cmd, MaxPipes);	
-		if (liste_cmd[0] == 0) return 0;				// ligne vide
+	decouper(pipeline, "|", liste_cmd, MaxPipes);
 
-		/* Traiter chaque commande de la ligne, sauf la dernière */
-		for (i = 0; liste_cmd[i+1] != 0; i++) {	
-			/* Exécuter une commande et récupérer le fichier qui servira d'entrée pour la suivante */
-			if ((in = traite_pipe(in, liste_cmd[i])) < 0) {
-				fprintf(stderr, "Problème dans l'exécution de la commande : %s.\n", liste_cmd[i]);
-				return -1; }
-		}
-
-		/* lancement du processus final de la ligne de commande */
-		if ((tmp = fork()) < 0){
-			perror("fork");
-			return -1;}
-
-		// parent : attendre la fin de l'enfant
-		if (tmp != 0){
-			while(wait(0) != tmp);
-			return 0;}
-
-		// enfant : exec du programme
-		if(in != 0) {
-			dup2(in, 0);		// récupère en entrée la sortie du dernier pipe
-			close(in); }
-		
-		decouper(liste_cmd[i], " \t\n", mot, MaxMot);
-		lance_cmd(mot);	// exécute la dernière commande de la ligne
-		return 0;
-}
-
-/* 	Lance un processus enfant qui lit depuis l'entrée spécifiée (in) et écrit dans un pipe
-	Renvoie le descripteur de fichier correspondant à la sortie du tube */
-int traite_pipe(int in, char * commande){
-	int pipe_fd[2];				
-	int cmd;	
-	char * mot[MaxMot];
-
-	if (decouper(commande, " \t\n", mot, MaxMot) == 0)	// commande vide
-	// if (strcmp(commande, " \0"))
-		return -1;
-
-	/* ouvrir un pipe pour communiquer avec le processus (frère) suivant */	
-	if (pipe(pipe_fd) != 0) {
-		perror("pipe");
-		return -1;	}
-
-	/* créer un enfant pour exécuter la "sous-commande" */
-	if ((cmd = fork()) < 0){
-		perror("fork");
-		return -1;}
-
-	if (cmd == 0){				// enfant
-		close(pipe_fd[0]); 		// ferme la sortie du tube (inutilisée)
-		dup2(pipe_fd[1], 1); 	// redirige sa sortie sur l'entrée du tube
-		close(pipe_fd[1]);
-		dup2(in, 0);			// redirige la sortie utilisée précédemment sur son entrée
-		close(in);
-		lance_cmd(mot);	// exécute la commande
+	/* plusieurs commandes successives */
+	if (liste_cmd[1] != 0) {
+		i = traite_pipe(liste_cmd);
+		if (i < 0) {
+			fprintf(stderr, "Echec de la commande [%d] : \"%s\".\n", -i, liste_cmd[-i]);
+			exit(-1); }
 	}
 
-	/* Parent */
-	close(pipe_fd[1]);			// fermeture de l'entrée du tube actuel
-	if (in != 0) close(in);		// fermeture de la sortie du tube précédent
-	return(pipe_fd[0]);
+	/* dernière (ou unique) commande de la pipeline */
+	lance_cmd(liste_cmd[i]);
+	return 0;
 }
+
+int traite_pipe(char * commandes[]){
+	int pipe_fd[2];				
+	int cmd, i;
+	int in = 0;
+
+	for (i = 0; commandes[i+1] != 0; i++) {	
+		if (est_vide(commandes[i])){
+			fprintf(stderr, "Erreur de syntaxe. ");		// fprintf ou pas nécessaire ?
+			return -i;}
+
+		/* ouvrir un pipe pour communiquer avec le processus suivant */	
+		if (pipe(pipe_fd) != 0) {
+			perror("pipe");
+			return -i;}
+
+		/* créer un enfant pour exécuter la "sous-commande" */
+		if ((cmd = fork()) < 0){
+			perror("fork");
+			return -i;}
+
+		if (cmd == 0){				// enfant
+			close(pipe_fd[0]); 		// ferme la sortie du tube (inutilisée)
+			dup2(pipe_fd[1], 1); 	// redirige sa sortie sur l'entrée du tube
+			close(pipe_fd[1]);
+			dup2(in, 0);			// redirige la sortie utilisée précédemment sur son entrée
+			close(in);
+			lance_cmd(commandes[i]);	// exécute la commande
+		}
+		/* Parent */
+		close(pipe_fd[1]);			// fermeture de l'entrée du tube actuel
+		if (in != 0) close(in);		
+		in = pipe_fd[0];
+	}
+
+	dup2(in, 0);		// récupère en entrée la sortie du dernier pipe
+	close(in); 
+	return i;
+	}
 
 
 /* exécute une commande, en cherchant l'exécutable dans la liste de répertoires fournie */
-int lance_cmd(char * mot[])
-{	char pathname[MaxPathLength];
-	
+int lance_cmd(char * commande){	
+	char * mot[MaxMot];				// liste des mots de la commande à exécuter
+	char pathname[MaxPathLength];
+
+	if (decouper(commande, " \t\n", mot, MaxMot) == 0){
+		fprintf(stderr, "Erreur de syntaxe (manque la dernière commande)\n");
+		exit(-1);}
+
 	for (int i = 0; path[i] != 0; i ++){
 		snprintf(pathname, sizeof pathname, "%s/%s", path[i], mot[0]);
 		execv(pathname, mot);
@@ -130,8 +140,8 @@ int lance_cmd(char * mot[])
 
 
 /* découpe une chaîne de caractères et renvoie le nombre de mots récupérés */
-int decouper(char * ligne, char * separ, char * mot[], int maxmot)
-{	int i ;
+int decouper(char * ligne, char * separ, char * mot[], int maxmot){	
+	int i ;
 	mot[0] = strtok(ligne, separ);
 	for(i = 1; mot[i-1] != 0; i ++){
 		if (i == maxmot){
@@ -142,4 +152,14 @@ int decouper(char * ligne, char * separ, char * mot[], int maxmot)
 		mot[i] = strtok(NULL, separ);
 	}
 	return (i-1);
+}
+
+/* Renvoie 1 si la chaîne de caractères ne contient que des espaces, 0 sinon */
+int est_vide(char *str) {
+    while (*str) {
+        if (!isspace(*str))
+            return 0;
+        str++;
+    }
+    return 1;
 }
